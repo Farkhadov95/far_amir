597d9ecbf7894ed564e70fca482a83f0ce3abdc0
Farkhadov95 <farkhadov950405@gmail.com>
2023-08-21 13:21:44 +0100

    Implemented Color Mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  ColorModeContext,
+  ColorModeScript,
+} from "@chakra-ui/react";
 import App from "./App";
 import { Route, RouterProvider } from "react-router-dom";
 import router from "./routing/routes";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
