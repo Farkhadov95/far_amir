e3223d912b29d95470eca4b1b1dcccb74b7097de
Farkhadov95 <farkhadov950405@gmail.com>
2023-08-29 22:58:55 +0100

    ScrollToTop component and scroll bug fix

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -4,11 +4,17 @@ import ResumePage from "../pages/ResumePage";
 import ProjectsPage from "../pages/ProjectsPage";
 import AboutPage from "../pages/AboutPage";
 import Layout from "../components/Layout";
+import ScrollToTop from "../components/ScrollToTop";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: (
+      <>
+        <ScrollToTop />
+        <Layout />
+      </>
+    ),
     children: [
       { path: "", element: <HomePage /> },
       { path: "resume", element: <ResumePage /> },
